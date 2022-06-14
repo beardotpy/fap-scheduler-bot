8c938cfade3d2d3352cdd0e8523cfdd47e4a3477
bear_ <captainkelvinvu@gmail.com>
2022-06-14 09:03:21 -0700

    he committed (add command and db functions)

--- db_functions.py
+++ db_functions.py
@@ -15,3 +15,19 @@ def add_fap(user):
         add_user(user)
     cursor.execute(f"INSERT INTO faps (user_id, date) VALUES (?, datetime('now'))", (user.id,))
     db.commit()
+
+def remove_fap(user, fap_id = None):
+    if not fap_id:
+        cursor.execute("DELETE FROM faps WHERE user_id = ? AND fap_id = (SELECT MAX(fap_id) FROM faps WHERE user_id = ?)", (user.id, user.id))
+        db.commit()
+    else:
+        cursor.execute("DELETE FROM faps WHERE user_id = ? AND fap_id = ?", (user.id, fap_id))
+        db.commit()
+
+    if cursor.rowcount == 0:
+        raise Exception()
+
+def get_faps(user=None):
+    if not user:
+        return cursor.execute("SELECT faps.fap_id, users.name, STRFTIME('%d/%m/%Y', faps.date), STRFTIME('%H:%M', faps.date) FROM faps, users WHERE faps.user_id = users.user_id").fetchall()
+    return cursor.execute("SELECT faps.fap_id, users.name, STRFTIME('%d/%m/%Y', faps.date), STRFTIME('%H:%M', faps.date) FROM faps, users WHERE faps.user_id = ? AND users.user_id = ?", (user.id, user.id)).fetchall()
--- fap_schedule.py
+++ fap_schedule.py
@@ -1,6 +1,6 @@
 import os
 import discord
-import sqlite3
+from tabulate import tabulate
 from db_functions import *
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -8,15 +8,53 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=["c!", "cum"], intents=discord.Intents.all())
+
+def remove_markdown(string):
+    special_chars = "*~_|`"
+    result = ""
+    for char in string:
+        if char in special_chars:
+            result += "\\"
+        result += char
+    return result    
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready.")
 
-@bot.command()
+@bot.command(aliases=["cum", "nut"])
 async def fap(ctx):
     add_fap(ctx.author)
-    await ctx.send("cummed")
+    await ctx.reply("You did the deed. Nice.")
+
+@bot.command(aliases=["unnut", "uncum"])
+async def unfap(ctx, fap_id=None):
+    try:
+        remove_fap(ctx.author, fap_id)
+    except:
+        await ctx.send("No fap to remove, fap id doesn't exist, or you tried to unfap someone else's fap.")
+    else:
+        await ctx.send(f"Unfapped.")
+
+@bot.command(aliases=["stat", "stats"])
+async def faps(ctx, member:discord.Member=None):
+    if member == None:
+        member = ctx.author
+    faps = get_faps(member)
+    if not faps:
+        await ctx.send("No fap data to show.")
+        return
+    table = tabulate(faps, headers=["Fap Id", "Coomer", "Date", "Time"], tablefmt="fancy_grid")
+    await ctx.send(f"```{table}```")
+
+@bot.command()
+async def allfaps(ctx):
+    faps = get_faps()
+    if not faps:
+        await ctx.send("No fap data to show.")
+        return
+    table = tabulate(faps, headers=["Fap Id", "Coomer", "Date", "Time"], tablefmt="fancy_grid")
+    await ctx.send(f"```{table}```")
 
 bot.run(TOKEN)
